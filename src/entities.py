16c936d93f166de08ffa0af8f4175b28a75fc8ed
Iñaki Silanes <isilanes@gmail.com>
2026-05-04 21:33:41 +0200

    Implement an initial loot drop event

--- src/entities.py
+++ src/entities.py
@@ -1,24 +1,36 @@
 from enum import Enum, auto
-from typing import Protocol, Self
 
 from pydantic import BaseModel
 
 
-class ObjectType(Enum):
-    GOOD_UNIQUE_LP0 = auto()
-    GOOD_UNIQUE_LP1 = auto()
-    GOOD_UNIQUE_LP2 = auto()
-    BAD_UNIQUE_LP0 = auto()
-    BAD_UNIQUE_LP1 = auto()
-    BAD_UNIQUE_LP2 = auto()
+class LootItem(Enum):
+    pass
+
+
+class Unique(LootItem):
+    GOOD_LP0 = auto()
+    GOOD_LP1 = auto()
+    GOOD_LP2 = auto()
+    BAD_LP0 = auto()
+    BAD_LP1 = auto()
+    BAD_LP2 = auto()
+
+
+class Legendary(LootItem):
+    GOOD_LP1 = auto()
+    GOOD_LP2 = auto()
+    BAD_LP1 = auto()
+    BAD_LP2 = auto()
 
 
 class Probabilities(BaseModel):
     """Object to provide the probability for all outcomes in all processes."""
 
-    unique_is_good: float
-    lp1: float
-    lp2: float = 0.0
+    unique_is_good: float        # probability that a Unique rolls as "good"
+    exalted_is_good: float       # probability that an Exalted (with proper affixes) rerolls as "good"
+    nemesis_stays_unique: float  # probability that the Unique in the Nemesis gets added LP, and not affixes
+    lp1: float                   # probability for {roll LP -> turns out LP1}
+    lp2: float = 0.0             # probability for {roll LP -> turns out LP2}
 
     @property
     def unique_is_bad(self) -> float:
@@ -28,41 +40,47 @@ class Probabilities(BaseModel):
     def lp0(self) -> float:
         return 1 - self.lp1 - self.lp2
 
-
-class CraftProcess(Protocol):
-
-    def __init__(self, probs: Probabilities) -> None:
-        self.probs = probs
-
-    def apply(self, pop: dict[ObjectType, float]) -> dict[ObjectType, float]:
-        pass
-
-
-class Drop(CraftProcess):
-
-    def __init__(self, probs: Probabilities) -> None:  # noqa
-        super().__init__(probs)
-
-    def apply(self, pop: dict[ObjectType, float]) -> dict[ObjectType, float]:
-        return {
-            ObjectType.GOOD_UNIQUE_LP0: self.probs.unique_is_good * self.probs.lp0,
-            ObjectType.GOOD_UNIQUE_LP1: self.probs.unique_is_good * self.probs.lp1,
-            ObjectType.GOOD_UNIQUE_LP2: self.probs.unique_is_good * self.probs.lp2,
-            ObjectType.BAD_UNIQUE_LP0: self.probs.unique_is_bad * self.probs.lp0,
-            ObjectType.BAD_UNIQUE_LP1: self.probs.unique_is_bad * self.probs.lp1,
-            ObjectType.BAD_UNIQUE_LP2: self.probs.unique_is_bad * self.probs.lp2,
-        }
+    @property
+    def exalted_is_bad(self) -> float:
+        return 1 - self.exalted_is_good
 
 
 class Population(BaseModel):
     """Holds amount of items of every object type."""
 
-    population: dict[ObjectType, float] = {}
-
-    def apply(self, craft: CraftProcess) -> Self:
-        self.population = craft.apply(self.population)
+    fractions: dict[LootItem, float] = {}
 
     def __str__(self) -> str:
-        return "\n".join([f"{k.name:15s}: {v:.3f}" for k, v in self.population.items()])
+        strings = [
+            "---\n",
+            "\n".join([f"UNIQUE:{item.name:10s}: {self.fractions.get(item, 0.0):6.3f}" for item in Unique]),
+            "\n\n",
+            "\n".join([f"LEGEND:{item.name:10s}: {self.fractions.get(item, 0.0):6.3f}" for item in Legendary]),
+        ]
+
+        return "".join(strings)
 
 
+def drop(probs: Probabilities, normalize: float = 1.0) -> Population:
+    """
+    Produce a Population of items according to the drop chances from monsters.
+
+    Args:
+        probs (Probabilities):
+            Probabilities object, holding probabilities for dropping each object type.
+        normalize (optional float):
+            If given, normalize total population to this number. By default, normalize to 1.
+
+    Returns:
+        A Population of object types.
+    """
+    return Population(
+        fractions={
+            Unique.BAD_LP0: probs.unique_is_bad * probs.lp0 * normalize,
+            Unique.BAD_LP1: probs.unique_is_bad * probs.lp1 * normalize,
+            Unique.BAD_LP2: probs.unique_is_bad * probs.lp2 * normalize,
+            Unique.GOOD_LP0: probs.unique_is_good * probs.lp0 * normalize,
+            Unique.GOOD_LP1: probs.unique_is_good * probs.lp1 * normalize,
+            Unique.GOOD_LP2: probs.unique_is_good * probs.lp2 * normalize,
+        },
+    )
--- src/operations.py
+++ src/operations.py
@@ -1,10 +1,26 @@
-from src.entities import Probabilities, Drop, Population
+from src.entities import Probabilities, drop
 
 
-probs = Probabilities(unique_is_good=0.013, lp1=0.2)
-drop = Drop(probs=probs)
-
-population = Population()
-population.apply(drop)
-
+probs = Probabilities(
+    unique_is_good=0.013,
+    exalted_is_good=0.1,
+    nemesis_stays_unique=0.25,
+    lp1=0.2,
+)
+population = drop(probs, 100)
 print(population)
+
+# nemesis = Nemesis(probs)
+# reroll_q = RerollQuality(probs)
+#
+# population.apply(reroll_q, LootItem.BAD_UNIQUE_LP0)
+# print(population)
+#
+# population.apply(nemesis, LootItem.GOOD_UNIQUE_LP0)
+# print(population)
+#
+# population.apply(reroll_q, LootItem.BAD_UNIQUE_LP1)
+# print(population)
+#
+# population.apply(reroll_q, LootItem.BAD_LEGENDARY_LP1)
+# print(population)
